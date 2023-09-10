8c175776008a64f7c479e48443d3743c96436fa5
Catrina K <33661922+catkos@users.noreply.github.com>
2023-09-10 12:08:02 +0300

    update
    added welcome and goodbye message

--- index.js
+++ index.js
@@ -9,25 +9,49 @@ app.use(express.static('public'));
 
 io.on('connection', (socket) => {
 
+  let selectedRoom = '1';
+  let username = '';
+
+  socket.on('user connected', (givenUsername) => {
+
+    //if empty username, name is anonymous with the last two characters of their ID
+    if(givenUsername){
+      username = givenUsername;
+    } else {
+      username = 'Anonyymi-'+socket.id.slice(-2);
+    }
+
+    // Join first room by default
+    socket.join(selectedRoom);
+    // Welcome message
+    io.emit('welcome', username + ' liittyi chattiin!');
+
+  });
+
   console.log('a user connected', socket.id);
 
   socket.on('disconnect', () => {
+    // on disconnect, emit that username left
+    io.emit('goodbye', username + ' poistui.');
     console.log('a user disconnected', socket.id);
   });
 
-  // Assign username (if no username, name is anonymous with the last two characters of id)
-  let username = 'Anonyymi-'+socket.id.slice(-2);
-  socket.on('register username', newUsername => {
-    username = newUsername;
+  // Get chat message, what room they're in, and who sent the message
+  socket.on('chat message', (msg, room, sender) => {
+
+    // Switch rooms, leave previous room
+    socket.on('join room', room => {
+      socket.leave(selectedRoom);
+      
+      socket.join(room);
+
+      selectedRoom = room;
+    });
+
+    // Emit chat message to room
+    io.to(room).emit('chat message', {msg, sender, username});
   });
 
-  socket.on('chat message', (msg) => {
-    console.log('message: ', msg);
-    // Get id of the user who sent the msg
-    let userID = socket.id;
-    // Add username to message
-    io.emit('chat message', {username, msg, userID});
-  });
 });
 
 http.listen(3000, () => {
