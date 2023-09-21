7e88cd6f03737bbb0b8272a430d8584ac67abf49
Catrina K <33661922+catkos@users.noreply.github.com>
2023-09-21 14:32:56 +0300

    Add files via upload

---
+++ my-first-bundle/graphics/index.js
@@ -0,0 +1,4 @@
+// You can access the NodeCG api anytime from the `window.nodecg` object
+// Or just `nodecg` for short. Like this!:
+nodecg.log.info("Here's an example of using NodeCG's logging API!");
+nodecg.Replicant("speakerReplicant", { defaultValue: "Maija Mehiläinen" });
