c754fe5fd100c5e99174c7b460e4337ac20646f7
Catrina K <33661922+catkos@users.noreply.github.com>
2023-09-29 19:31:00 +0300

    Add files via upload

--- my-first-bundle/dashboard/panel.js
+++ my-first-bundle/dashboard/panel.js
@@ -1,3 +1,26 @@
 // You can access the NodeCG api anytime from the `window.nodecg` object
 // Or just `nodecg` for short. Like this!:
 nodecg.log.info("Here's an example of using NodeCG's logging API!");
+
+const speakerRep = nodecg.Replicant("speakerReplicant");
+const speaker = document.getElementById("speaker");
+const roleRep = nodecg.Replicant("roleReplicant");
+const role = document.getElementById("role");
+
+speakerRep.on("change", (newValue, oldValue) => {
+  console.log(`myRep changed from ${oldValue} to ${newValue}`);
+  speaker.value = newValue;
+});
+
+roleRep.on("change", (newValue, oldValue) => {
+	console.log(`myRep changed from ${oldValue} to ${newValue}`);
+	role.value = newValue;
+  });
+
+const speakerForm = document.getElementById("speakerForm");
+
+speakerForm.addEventListener("submit", (e) => {
+  e.preventDefault;
+  speakerRep.value = speaker.value;
+  roleRep.value = role.value;
+});
--- my-first-bundle/graphics/index.js
+++ my-first-bundle/graphics/index.js
@@ -1,4 +1,46 @@
 // You can access the NodeCG api anytime from the `window.nodecg` object
 // Or just `nodecg` for short. Like this!:
 nodecg.log.info("Here's an example of using NodeCG's logging API!");
-nodecg.Replicant("speakerReplicant", { defaultValue: "Maija Mehiläinen" });
+
+nodecg.Replicant("speakerReplicant", { defaultValue: "Tom Nook" });
+nodecg.Replicant("roleReplicant", { defaultValue: "Real Estate Agent" });
+
+const speakerRep = nodecg.Replicant("speakerReplicant");
+const puhuja = document.getElementById("puhuja");
+const roleRep = nodecg.Replicant("roleReplicant");
+const rooli = document.getElementById('rooli');
+
+speakerRep.on("change", (newValue, oldValue) => {
+  console.log(`myRep changed from ${oldValue} to ${newValue}`);
+  puhuja.innerHTML = newValue;
+  nameAnimation();
+});
+
+roleRep.on("change", (newValue, oldValue) => {
+  console.log(`myRep changed from ${oldValue} to ${newValue}`);
+  rooli.innerHTML = newValue;
+  roleAnimation();
+});
+
+const blokki = document.getElementById("blokki");
+const tokablokki = document.getElementById("tokablokki");
+
+function nameAnimation() {
+  blokki.classList.remove("title-block-animation");
+  blokki.offsetWidth;
+  blokki.classList.add("title-block-animation");
+
+  puhuja.classList.remove("title-h1-animation");
+  puhuja.offsetWidth;
+  puhuja.classList.add("title-h1-animation");
+}
+
+function roleAnimation() {
+  tokablokki.classList.remove("role-block-animation");
+  tokablokki.offsetWidth;
+  tokablokki.classList.add("role-block-animation");
+
+  rooli.classList.remove("role-p-animation");
+  rooli.offsetWidth;
+  rooli.classList.add("role-p-animation");
+}
